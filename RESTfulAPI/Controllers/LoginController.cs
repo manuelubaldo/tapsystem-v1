7fee8f6e47406b67b05a340b4a5662a83ae3341d
manuelubaldo <klauel.ubaldo@gmail.com>
2016-10-02 14:14:35 +0800

    Add Login Controller

---
+++ RESTfulAPI/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using RESTfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTfulAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private TapSystemEntities db = new TapSystemEntities();
+        public IHttpActionResult Post(tblUser User)
+        {
+
+            var result = from u in db.tblUsers
+                         where u.tUserName == User.tUserName && u.tPassword == User.tPassword
+                         select u;
+
+            if (result.Count()<1)
+            {
+                return Ok(new tblUser());
+            }
+            else
+            {
+                return Ok(result.First());
+            }
+        }
+    }
+}
