09a8e760072a8499058228506fb0de9193ed9bf7
manuelubaldo <klauel.ubaldo@gmail.com>
2016-10-05 02:23:36 +0800

    New controller to cater bus assignment

---
+++ RESTfulAPI/Controllers/BusAssignmentController.cs
@@ -0,0 +1,46 @@
+﻿using RESTfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RESTfulAPI.Controllers
+{
+    public class BusAssignmentController : ApiController
+    {
+        private TapSystemEntities db = new TapSystemEntities();
+
+        public IHttpActionResult Get(int id)
+        {
+            var busAssignment = from b in db.tblBusAssignments
+                                where b.iConductorID == id && b.bActive == 1
+                                select new BusAssignmentDTO
+                                {
+                                    iBusID = b.iBusID,
+                                    tBusPlateNo = b.tblBus.tPlateNo,
+                                    iConductorID = b.iConductorID,
+                                    tConductionName = b.conductor.tFullName,
+                                    iDriveID = b.iDriverID,
+                                    tDriverName = b.driver.tFullName,
+                                    iInspectorID = b.iInspector,
+                                    tInspectorName = b.inspector.tFullName,
+                                    iRouteID = b.iRouteID,
+                                    tRouteName = b.tblRoute.tFrom + " to " + b.tblRoute.tTo
+                                };
+
+            if(busAssignment.Count()>0)
+            {
+                return Ok(busAssignment.First());
+            }
+            else
+            {
+                return Ok(new BusAssignmentDTO());
+            }
+
+            
+        }
+
+    }
+}
