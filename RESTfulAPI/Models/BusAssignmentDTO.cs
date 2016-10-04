f19e26f61d8c2684e3601b23104877eb284ed5d2
manuelubaldo <klauel.ubaldo@gmail.com>
2016-10-05 02:22:18 +0800

    Create DTO Class

---
+++ RESTfulAPI/Models/BusAssignmentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTfulAPI.Models
+{
+    public class BusAssignmentDTO
+    {
+        public int iConductorID { get; set; }
+        public String tConductionName { get; set; }
+        public int iDriveID { get; set; }
+        public String tDriverName { get; set; }
+        public int iInspectorID { get; set; }
+        public String tInspectorName { get; set; }
+        public int iBusID { get; set; }
+        public String tBusPlateNo { get; set; }
+        public int iRouteID { get; set; }
+        public String tRouteName { get; set; }
+    }
+}
